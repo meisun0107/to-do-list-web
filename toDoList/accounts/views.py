006ef46cb5d8ebd648d4249bfff10863c7d59895
Mei Sun <68130227+meisun0107@users.noreply.github.com>
2022-05-14 11:57:28 -0400

    add user login & logout

--- toDoList/accounts/views.py
+++ toDoList/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from .forms import UserLoginForm
+from django.urls import reverse
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        user_login_form = UserLoginForm(data=request.POST)
+        if user_login_form.is_valid():
+            data = user_login_form.cleaned_data
+            user = authenticate(username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return redirect(reverse("home:index"))
+            else:
+                return HttpResponse("Wrong username or password, please try again.")
+        else:
+            return HttpResponse("Invaild username or password.")
+    elif request.method == 'GET':
+        user_login_form = UserLoginForm()
+        context = { 'form': user_login_form }
+        return render(request, 'accounts/templates/login.html', context)
+    else:
+        return HttpResponse("请使用GET或POST请求数据")
+
+def user_logout(request):
+    logout(request)
+    return redirect(reverse("home:index"))
